777206b1f6788295c4d2b4060f1ff2bb2a3dfb15
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2019-08-11 12:10:53 -0300

    fReusing instantiation

--- src/Program.java
+++ src/Program.java
@@ -12,6 +12,7 @@ public class Program {
 		
 		//Seller seller = new Seller(21, "Bob","bobin@gmail.com", new Date(), 3000.0, obj);
 		
+		System.out.println("=== Teste 1: seller findById ===");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
 		
