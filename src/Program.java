27e2231481dfd48d87452550a688c4a8b5642b2d
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2019-08-11 11:14:20 -0300

    Seller entity class

--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,14 @@
-import model.entities.*;
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
 	
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob","bobin@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 
 	}
 
