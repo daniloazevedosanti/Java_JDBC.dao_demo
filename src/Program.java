6b780b995b8318a27c4320a78c53767f72493495
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2019-08-17 14:55:57 -0300

    update implementation

--- src/Program.java
+++ src/Program.java
@@ -27,8 +27,14 @@ public class Program {
 		
 		System.out.println("\n=== Teste 4: seller insert ===");
 		Seller newSeller = new Seller (null, "GregHowie", "gregh@gmail.com", new Date(), 4000.00, department);
-		sellerDao.insert(newSeller);
+		//sellerDao.insert(newSeller);
 		System.out.println("Inserted! New ID = "+ newSeller.getId());
+		
+		System.out.println("\n=== Teste 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Complete!");
 	}
 
 }
